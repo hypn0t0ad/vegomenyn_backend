c3db73731ea7cfb529700bb4ca8ed53b1c3041c1
christian dahlgren <christian.dahlgren84@gmail.com>
2022-05-19 23:34:05 +0200

    it works

--- models/CourseModel.js
+++ models/CourseModel.js
@@ -6,8 +6,7 @@ const CourseSchema = new mongoose.Schema({
     image: { type: String, required: false },
     price: { type: Number, required: true },  
     description: { type: String, required: false, maxlength: 500 },
-    //foodtypes: [{ type: Schema.Types.ObjectId, ref: 'Foodtypes' }],
-    foodtypes: [{ type: String }],
+    foodtypes: [{ type: Schema.Types.ObjectId, ref: 'Foodtypes' }],
     dateUpdated: { type: Date, default: Date.now() }
 });
 
